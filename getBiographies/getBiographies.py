863cb14af83eb5d82563e8144ac7b360cc56081a
jayway <jga26@njit.edu>
2024-01-22 00:03:51 -0500

    comments and formatting

--- getBiographies/getBiographies.py
+++ getBiographies/getBiographies.py
@@ -2,24 +2,21 @@ import json
 import boto3
 import decimal
 
+
 def lambda_handler(event, context):
     """
     Access biographies table from DynamoDB resource
     and return all items in the table
     """
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('njitsbc-biographies')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("njitsbc-biographies")
     response = table.scan()
-    items = response['Items']
+    items = response["Items"]
 
     # handle Decimal type
-
     for item in items:
         for key in item:
             if isinstance(item[key], decimal.Decimal):
                 item[key] = int(item[key])
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps(items)
-    }
+    return {"statusCode": 200, "body": json.dumps(items)}
