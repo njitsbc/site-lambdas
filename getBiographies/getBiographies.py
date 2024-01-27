4dda00586279266a0356565e0f72c1df9e147f44
Mostafa Damir <mad237@njit.edu>
2024-01-27 11:54:49 -0500

    added the ability to get pending bios
    Signed-off-by: Mostafa Damir <mad237@njit.edu>

--- getBiographies/getBiographies.py
+++ getBiographies/getBiographies.py
@@ -2,6 +2,11 @@ import json
 import boto3
 import decimal
 
+paths = {
+    "/getPendingBiographies": "njitsbc-pendingBiographies",
+    "/getBiographies": "njitsbc-biographies",
+}
+
 
 def lambda_handler(event, context):
     """
@@ -9,11 +14,17 @@ def lambda_handler(event, context):
     and return all items in the table
     """
     dynamodb = boto3.resource("dynamodb")
-    table = dynamodb.Table("njitsbc-biographies")
+
+    raw_path = event["rawPath"]
+    if raw_path not in paths:
+        return {"statusCode": 400, "message": f"{raw_path} is not a supported endpoint."}
+    table = dynamodb.Table(paths[raw_path])
+
     response = table.scan()
     items = response["Items"]
 
     # handle Decimal type
+
     for item in items:
         for key in item:
             if isinstance(item[key], decimal.Decimal):
